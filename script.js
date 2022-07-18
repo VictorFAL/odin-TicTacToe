649101da954f0ad2f25ea0f073f748461e7613a3
Victor | Harry <victorfalmeida12@gmail.com>
2022-07-18 19:29:00 -0300

    rendering + turns added

--- script.js
+++ script.js
@@ -1,4 +1,64 @@
+const spots = document.querySelectorAll('td');
+
 const gameBoard = (() => {
     'use strict';
-    let _board = Array(9);
+
+    let board = Array(9);
+
+    spots.forEach(spot => {
+        spot.addEventListener('click', () => {
+            if(spot != "") {
+                if(playerX.active) {
+                    playerX.play(spot.dataset.index);
+                    playerX.active = false;
+                    playerO.active = true;
+
+                    game.render();
+                } 
+                else if(playerO.active) {
+                    playerO.play(spot.dataset.index);
+                    playerO.active = false;
+                    playerX.active = true;
+                    
+                    game.render();
+                }
+            }
+        });
+    });
+
+    return {board}
 })();
+
+
+const game = (() => {
+    'use strict';
+
+    const render = () => {
+        gameBoard.board.forEach((item, index) => {
+            spots[index].innerText = item;
+        });
+    }
+
+    return {render}
+})();
+
+
+const player = (mark) => {
+    let active = true;
+
+    const play = (spot) => {
+        gameBoard.board[spot] = mark;
+    }
+
+    return {active, play};
+}
+
+const playerX = player('X');
+const playerO = player('O');
+
+playerO.active = false;
+
+// test
+// gameBoard.board[0] = 'X';
+// gameBoard.board[4] = 'O';
+// game.render();
