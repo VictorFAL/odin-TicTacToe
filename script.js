c8f66cf1864e7eac83ff3f9ae2600cc304f99977
Victor | Harry <victorfalmeida12@gmail.com>
2022-07-22 11:53:35 -0300

    win condition added

--- script.js
+++ script.js
@@ -5,6 +5,7 @@ const gameBoard = (() => {
     'use strict';
 
     let board = Array(9);
+    board.fill(undefined);
 
     spots.forEach(spot => {
         spot.addEventListener('click', () => {
@@ -13,18 +14,19 @@ const gameBoard = (() => {
                     playerX.play(spot.dataset.index);
                     playerX.active = false;
                     playerO.active = true;
-
+    
                     game.render();
-                } 
-                else if(playerO.active) {
+                    game.checkWin();
+                } else if(playerO.active) {
                     playerO.play(spot.dataset.index);
                     playerO.active = false;
                     playerX.active = true;
                     
                     game.render();
+                    game.checkWin();
                 }
             }
-        });
+        }, {once: true});
     });
 
     return {board}
@@ -37,15 +39,57 @@ const game = (() => {
 
     const render = () => {
         gameBoard.board.forEach((item, index) => {
-            spots[index].innerText = item;
+            if(item != undefined) {
+                spots[index].innerText = item;
+            }
         });
     }
 
-    const checkWin = (mark) => {
-        const condition = [];
+    const checkWin = () => {
+        const winConditions = [
+            // Horizontal
+            [0, 1, 2],
+            [3, 4, 5],
+            [6, 7, 8],
+            // Vertical
+            [0, 3, 6],
+            [1, 4, 7],
+            [2, 5, 8],
+            // Diagonal
+            [0, 4, 8],
+            [2, 4, 6]
+        ];
+
+        let board = gameBoard.board;
+        let victory = false;
+        for(let i = 0; i <= 7; i++) {
+            let winCond = winConditions[i];
+            let spot1 = winCond[0];
+            let spot2 = winCond[1];
+            let spot3 = winCond[2];
+
+            if(board[spot1] == undefined || board[spot2] == undefined || board[spot3] == undefined) {
+                continue;
+            }
+            if(board[spot1] == board[spot2] && board[spot2] == board[spot3]) {
+                victory = true;
+
+                spots[spot1].style.backgroundColor = 'rgb(161, 228, 161)';
+                spots[spot2].style.backgroundColor = 'rgb(161, 228, 161)';
+                spots[spot3].style.backgroundColor = 'rgb(161, 228, 161)';
+
+                break;
+            }
+        }
+
+        if(victory) {
+            spots.forEach((spot) => {
+                spot.classList.add('activated');
+            });
+        }
     }
 
-    return {render}
+    return {render, checkWin}
 })();
 
 
